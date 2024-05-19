2e72d10d1b8441012af29714d07f48ce0b50321f
Anish-Behuray <111987913+Anish-Behuray@users.noreply.github.com>
2024-05-20 01:29:45 +0530

    Update AddDatatoDatabase.py

--- AddDatatoDatabase.py
+++ AddDatatoDatabase.py
@@ -4,7 +4,7 @@ from firebase_admin import db
 
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred, {
-    'databaseURL': "https://face-recognition-3f2a7-default-rtdb.firebaseio.com/"
+    'databaseURL': ""
 })
 
 
@@ -12,67 +12,20 @@ firebase_admin.initialize_app(cred, {
 ref = db.reference('Students')
 
 data = {
-    "123456":
+    "id1":
         {
-            "name": "Anish Behuray",
-            "major": "Civil",
-            "roll_number": '21CE30006',
-            "total_attendance": 7,
-            "standing": "G",
-            "year": 4,
-            "last_attendance_time": "2022-12-11 00:54:34"
+            "name": "",
+            "major": "",
+            "roll_number": '',
+            "total_attendance": ,
         },
-    "13579":
+    "id2":
         {
-            "name": "Minati Behuray",
-            "major": "Everything",
-            "roll_number": '21CE30006',
-            "total_attendance": 7,
-            "standing": "G",
-            "year": 4,
-            "last_attendance_time": "2022-12-11 00:54:34"
-        },
-    "321654":
-        {
-            "name": "Murtaza Hassan",
-            "major": "Robotics",
-            "roll_number": '21CE30006',
-            "total_attendance": 7,
-            "standing": "G",
-            "year": 4,
-            "last_attendance_time": "2022-12-11 00:54:34"
-        },
-    # "654321":
-    #     {
-    #         "name": "Soumyashree Nayak",
-    #         "major": "Mechnics",
-    #         "roll_number": '21CE30006',
-    #         "total_attendance": 7,
-    #         "standing": "G",
-    #         "year": 4,
-    #         "last_attendance_time": "2022-12-11 00:54:34"
-    #     },
-    "852741":
-        {
-            "name": "Emly Blunt",
-            "major": "Economics",
-            "roll_number": '21CE30006',
-            "total_attendance": 12,
-            "standing": "B",
-            "year": 1,
-            "last_attendance_time": "2022-12-11 00:54:34"
-        },
-    "963852":
-        {
-            "name": "Elon Musk",
-            "major": "Physics",
-            "roll_number": '21CE30006',
-            "total_attendance": 7,
-            "standing": "G",
-            "year": 2,
-            "last_attendance_time": "2022-12-11 00:54:34"
+            "name": "",
+            "major": "",
+            "roll_number": '',
+            "total_attendance": ,
         }
-
 
 }
 
